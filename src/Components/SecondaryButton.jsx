1b23ad5cf54fb752e1d8ca163eeb8cbaa630f406
Tarunna Sen <135026838+T4RUNN4@users.noreply.github.com>
2025-10-25 20:37:46 +0600

    Delete src directory

--- src/Components/SecondaryButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function SecondaryButton({ text, type="button", onClick }) {
-  return (
-    <button type={type} className="px-3 pt-2 pb-1 border-2 rounded-lg bg-[#040b1a] text-[#e5e7eb] text-base cursor-pointer hover:bg-red-600 transition duration-200" onClick={onClick}>
-      {text}
-    </button>
-  );
-}

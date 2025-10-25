b4d4cb3d9bcce5a17607e3f173a52a4da2b8ad54
Tarunna Sen <135026838+T4RUNN4@users.noreply.github.com>
2025-10-25 20:51:53 +0600

    Update user.jsx

--- src/Pages/user.jsx
+++ src/Pages/user.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from "@Sections/Header";
 import Footer from "@Sections/Footer";
 import Logo from "@assets/logo.png";
@@ -25,31 +24,3 @@ export default function User() {
     </div>
   );
 }
-=======
-import Header from "../Sections/Header";
-import Footer from "../Sections/Footer";
-import Logo from "../assets/logo.png";
-import ProductsCatalog from "../Sections/ProductCatalog";
-import CartAndMemo from "../Sections/User/CartAndMemo";
-import ItemRequest from "../Sections/User/ItemRequest";
-import MemoCatalog from "../Sections/MemoCatalog";
-
-export default function User() {
-  return (
-    <div className="min-h-screen w-full text-[#e5e7eb] text-sm bg-gradient-to-b from-[#0b1024] to-[#020617]">
-      <Header Logo={Logo} />
-      <main className="w-full grid grid-cols-1 md:grid-cols-[1fr_360px] gap-4 p-4">
-        <div className="space-y-4">
-          <ProductsCatalog />
-          <CartAndMemo />
-        </div>
-        <div className="space-y-4">
-          <MemoCatalog />
-          <ItemRequest />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
-}
->>>>>>> 475bb9ce060e4db2334f57b152ed24cd86d8b11a

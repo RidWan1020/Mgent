fb441a7a093c399b786ea0fb6c2219d91cdd918b
Tarunna <tarunnasen@gmail.com>
2025-12-05 12:42:11 +0600

    Redirecting Logo

--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Dropdown from "@Components/Menu";
 
 export default function Header({ Logo }) {
+  const navigate = useNavigate();
 
   return (
     <header className="flex items-center justify-between gap-2.5 px-3.5 py-4 backdrop-blur-sm bg-[#040b1a] text-white sticky top-0 border-b border-[#1f2937]">
-        <img src={Logo} alt="Logo" className="h-10" />
-        <Dropdown />
+      <img
+        src={Logo}
+        alt="Logo"
+        className="h-10 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+
+      <Dropdown />
     </header>
   );
 }
